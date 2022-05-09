a3d793625be9f0f9a2adccc75c7722c30722c559
Carlos-Lemus195 <202000195@udv.edu.gt>
2022-05-09 08:42:10 -0600

    Se agregó la feature para verificar si el email recibido se encuentra en la colección clintes. Y se modificó el feature luhn el cual verifica si el número de tarjeta de crédito es válido o no.

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,14 @@ const luhnAPI = require('./luhn.js')
 const registroEmpleadosAPI = require('./registro_empleados.js')
 const registroClientesAPI = require('./registro_clientes.js')
 const loginAPI = require('./login.js')
+const verifyEmailAPI = require('./verifyEmail.js')
 
 function  controllers(app) {
     luhnAPI(app);
     registroEmpleadosAPI(app);
     registroClientesAPI(app);
     loginAPI(app);
+    verifyEmailAPI(app);
 }
 
 module.exports = controllers;
