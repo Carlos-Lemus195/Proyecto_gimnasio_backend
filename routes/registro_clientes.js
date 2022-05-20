310a784f248145c329bfeade7620bfab9111cacb
Kevin Rosales <kevinrosales1511@gmail.com>
2022-05-20 00:01:36 -0600

    Tabla de clientes, implementacion metodo delete, update, post

--- routes/registro_clientes.js
+++ routes/registro_clientes.js
@@ -10,19 +10,75 @@ function registroAPI(app) {
     const registroService = new RegistroService();
 
     router.put("/", async function(req, res, next){
-        const { query: datos } = req;
+        const { body: datos } = req;
         try {
-            const remainingdata = { "mensualidad": 250, "moroso": "no", "montoMora": 0, "estado": "activo"};
+            //Esto es lo que te comentaba de la fechaPago
+            let day = new Date().getDate();
+            let month = new Date().getMonth() + 1;
+            if (month < 10) { month = '0' + month.toString() };
+            if (day < 10) { day = '0' + day.toString() };
+            const year = new Date().getFullYear();
+            const fecha = year.toString() + '-' + month + '-' + day;
+            //Acá termina
+
+                                                                                                                           //Agrega lo de fechaPago
+            const remainingdata = { "mensualidad": 250, "moroso": "no", "montoMora": 0, "estado": "activo", "cantUltimoPago": 0, "fechaPago": fecha};
             const data = Object.assign(datos,remainingdata);
             const registroCreated = await registroService.createRegistro(data);
             res.status(200).json({
                 data: registroCreated,
-                message: 'data succesfully inserted in Clientes'
+                message: 'data succesfully inserted in Clientes' 
+            });
+        } catch(err) {
+            res.status(200).json({
+                message: 'the data was not inserted in Clientes'
+            });
+            next(err);
+        }
+    });
+    router.get("/", async function(req, res, next){
+        try {
+            const clientes = await registroService.getClientes();
+            res.status(200).json({
+                clientes: clientes,
+                message: 'rutina collection requested successfully'
+            });
+        } catch (error) {
+            next(error);
+        }
+    });
+
+    router.delete("/", async function(req, res, next){
+        const { body: data } = req;
+
+        try {
+            const clienteDeleted = await registroService.deleteCliente(data);
+            res.status(200).json({
+                data: clienteDeleted,
+                message: 'Cliente succesfully deleted'
             });
 
         } catch(err) {
             res.status(200).json({
-                message: 'the data was not inserted in Clientes'
+                message: 'Cliente not deleted'
+            });
+            next(err);
+        }
+    });
+
+    router.post("/", async function(req, res, next){
+        const { body: data } = req;
+
+        try {
+            const clienteUpdated = await registroService.updateCliente(data);
+            res.status(200).json({
+                data: clienteUpdated,
+                message: 'Cliente succesfully updated'
+            });
+
+        } catch(err) {
+            res.status(200).json({
+                message: 'Cliente not updated'
             });
             next(err);
         }
--- services/registro_clientes.js
+++ services/registro_clientes.js
@@ -12,6 +12,23 @@ class RegistroService {
         return registroCreated;
     }
 
+    async getClientes() {
+        const clientes = await this.mongoDB.getTodos(this.collection);
+        return clientes || {};
+    }
+
+    async updateCliente(data) {
+        const clienteUpdated = await this.mongoDB.updateCliente(this.collection, data.id, data);
+        console.log('cliente Updated', clienteUpdated);
+        return clienteUpdated || {};
+    }
+
+    async deleteCliente(id) {
+        const clienteDeleted = await this.mongoDB.delete(this.collection, id);
+        console.log('Cliente deleted', clienteDeleted);
+        return clienteDeleted || {};
+    }
+
 }
 
 module.exports = RegistroService;
